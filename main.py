a5e65225d830fcedb54567512abc4ffd53f7e503
Tanya <tetiana.nazarova.kn.2021@lpnu.ua>
2022-09-27 14:07:39 +0300

    Change

--- main.py
+++ main.py
@@ -1,10 +1,3 @@
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+print('hi')
 
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-print("hi")
 
